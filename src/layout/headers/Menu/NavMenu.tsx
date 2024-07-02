ccd5db9c738e25a4e1597de069a5080b29e28ba0
rizwan601 <rizwanramzan1196@gmail.com>
2024-07-02 13:00:34 +0500

    add active color

--- src/layout/headers/Menu/NavMenu.tsx
+++ src/layout/headers/Menu/NavMenu.tsx
@@ -1,47 +1,98 @@
 "use client";
-import Link from "next/link.js";
+import { useState, useEffect } from "react";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 import menu_data from "../../../data/MenuData";
 
 const NavMenu = () => {
     const currentRoute = usePathname();
+    const [activeMenuItem, setActiveMenuItem] = useState(currentRoute);
+
+    useEffect(() => {
+        setActiveMenuItem(currentRoute);
+    }, [currentRoute]);
+
+    const handleMenuItemClick = (menuLink: string) => {
+        setActiveMenuItem(menuLink);
+    };
 
     const isMenuItemActive = (menuLink: string) => {
-        return currentRoute === menuLink;
+        return activeMenuItem === menuLink;
     };
 
     const isSubMenuItemActive = (subMenuLink: string) => {
-        return currentRoute === subMenuLink;
+        return activeMenuItem === subMenuLink;
     };
+
+    const handleScroll = () => {
+        const sections = document.querySelectorAll("section");
+        let currentSectionId = "";
+
+        sections.forEach((section) => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+            if (window.scrollY >= sectionTop - sectionHeight / 2) {
+                currentSectionId = section.getAttribute("id") || "";
+            }
+        });
+
+        if (currentSectionId) {
+            const activeMenu = menu_data.find((menu) => menu.link === `/#${currentSectionId}`);
+            if (activeMenu) {
+                setActiveMenuItem(activeMenu.link);
+            } else {
+                menu_data.forEach((menu) => {
+                    if (menu.sub_menus) {
+                        const activeSubMenu = menu.sub_menus.find(
+                            (subMenu) => subMenu.link === `/#${currentSectionId}`
+                        );
+                        if (activeSubMenu) {
+                            setActiveMenuItem(activeSubMenu.link);
+                        }
+                    }
+                });
+            }
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
 
     return (
         <ul className="navigation">
-            {menu_data.map((menu: any) => (
-                <li key={menu.id}
+            {menu_data.map((menu) => (
+                <li
+                    key={menu.id}
                     className={menu.has_dropdown ? "menu-item-has-children" : ""}
                 >
-                    <Link href={menu.link}
-                        className={`section-link ${(isMenuItemActive(menu.link) || (menu.sub_menus && menu.sub_menus.some((sub_m: any) => sub_m.link && isSubMenuItemActive(sub_m.link)))) ? "active" : ""}`}>
+                    <Link
+                        href={menu.link}
+                        className={`section-link ${
+                            isMenuItemActive(menu.link) ? "active" : ""
+                        }`}
+                        onClick={() => handleMenuItemClick(menu.link)}
+                    >
                         {menu.title}
                     </Link>
 
                     {menu.has_dropdown && (
-                        <>
-                            {menu.sub_menus && (
-                                <ul className="sub-menu">
-                                    {menu.sub_menus.map((sub_m: any, i: any) => (
-                                        <li key={i}>
-                                            <Link
-                                                href={sub_m.link}
-                                                className={
-                                                    sub_m.link && isSubMenuItemActive(sub_m.link) ? "active" : ""}>
-                                                {sub_m.title}
-                                            </Link>
-                                        </li>
-                                    ))}
-                                </ul>
-                            )}
-                        </>
+                        <ul className="sub-menu">
+                            {menu.sub_menus?.map((sub_m, i) => (
+                                <li key={i}>
+                                    <Link
+                                        href={sub_m.link}
+                                        className={isSubMenuItemActive(sub_m.link) ? "active" : ""}
+                                        onClick={() => handleMenuItemClick(sub_m.link)}
+                                    >
+                                        {sub_m.title}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
                     )}
                 </li>
             ))}
@@ -50,4 +101,3 @@ const NavMenu = () => {
 };
 
 export default NavMenu;
-
